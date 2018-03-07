f45cb29e2e7c0794b71f4e7fac81f78cd56c3b64
Przemysław Kalita <Przemysaw Kalita>
2018-03-07 18:10:19 +0100

    installed mongoose and added post

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+mongoose.connect('mongodb://localhost/test');
+const TemperatureReading = mongoose.model('TemperatureReading', { temp1: String, temp2: String, temp3: String, timeStamp: String });
+
+
+var bodyParser = require('body-parser')
+
+const app = express()
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/', (req, res) => res.send('Works!'))
+
+app.post('/', function (req, res) {
+    var date = new Date();
+    const tempReading = new TemperatureReading({
+        temp1: req.body.temp1,
+        temp2: req.body.temp2,
+        temp3: req.body.temp3,
+        timeStamp: date.toString() });
+
+    tempReading.save().then(() => console.log('Saved reading'));
+
+
+    console.log(req.body);
+    res.send();
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
