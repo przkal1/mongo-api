6ff83b838343d95482c925b51a4e70adfa705b25
Przemysław Kalita <Przemysaw Kalita>
2018-03-22 21:40:01 +0100

    debug api

--- index.js
+++ index.js
@@ -64,6 +64,8 @@ app.get('/historical', function (req, res) {
 app.get('/test', function (req, res) {
 
     TemperatureReading.find({ }, 'temp1 temp2 temp3 timeStamp', { sort: { 'timeStamp' : -1 }, limit: 100 }, function (err, temperatures) {
+
+        console.log(temperatures);
         var groupedData = _.groupBy(temperatures, function(temperature) { return new moment(temperature.timeStamp).format('YYYY-MM-DD HH'); });
         res.send(groupedData);
 
